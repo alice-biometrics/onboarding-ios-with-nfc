b7173c8bf4c1c5c6aaa258111eee5c704d6a4b61
garciagarciaadrian <agarcia@alicebiometrics.com>
2024-01-09 10:02:49 +0100

    chore: add package.swift

---
+++ Package.swift
@@ -0,0 +1,37 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let version = "2.0.0"
+let checksum = "7f4eb36c29d0a0d6cd774a88be08964f8e4023198a153094901dd6a0f8139729"
+
+let package = Package(
+    name: "AliceOnboardingWithNFC",
+    platforms: [
+        .iOS(.v9)
+    ],
+    products: [
+        .library(
+            name: "AliceOnboardingWithNFC",
+            targets: ["AliceOnboardingBundle"]),
+    ],
+    dependencies: [
+            .package(
+                name: "OpenSSL",
+                url: "https://github.com/krzyzanowskim/OpenSSL.git",
+                .exact("1.1.2000")
+            )
+    ],
+    targets: [
+      .target(name: "AliceOnboardingBundle",
+            dependencies: [
+                "AliceOnboarding",
+                .product(name: "OpenSSL", package: "OpenSSL")
+              ]
+           ),
+        .binaryTarget(name: "AliceOnboardingWithNFC", 
+                      url: "https://storage.googleapis.com/alicebiometrics.com/releases/ios/AliceOnboardingWithNFC-\(version).zip",
+                      checksum: "\(checksum)"),
+    ]
+)
